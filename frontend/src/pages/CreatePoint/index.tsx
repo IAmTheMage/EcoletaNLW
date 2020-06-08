d553300ec959d1365615a62f9205dd5e6c1b56e9
IAmTheMage123 <gustavogoncalvessilva2003@outlook.com>
2020-06-08 20:06:45 -0300

    Add map

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Dropzone from "../../components/Dropzone";
 import { FiArrowLeft } from "react-icons/fi";
+import { Map, TileLayer, Marker } from "react-leaflet";
+import { LeafletMouseEvent } from "leaflet";
 import "./styles.css";
 
 import logo from "../../assets/svg/logo.svg";
 import api from "../../services/api";
+import ibge from "../../services/ibge";
 
 interface Item {
   id: number;
@@ -13,18 +16,101 @@ interface Item {
   image: string;
 }
 
+interface IBGEUFS {
+  id: number;
+  sigla: string;
+  name: string;
+  region: {
+    id: number;
+    sigla: string;
+    name: string;
+  };
+}
+
+interface IBGECITIES {
+  id: number;
+  name: string;
+}
+
+interface LATLNG {
+  lat: number;
+  lng: number;
+}
+
 const CreatePoint = () => {
   const [file, setFile] = useState<any>();
   const [selectedFile, setSelectedFile] = useState<File>();
   const [items, setItems] = useState<Item[]>([]);
-
+  const [selectedItemsArray, setSelectedItemsArray] = useState<number[]>([]);
+  const [ufs, setUfs] = useState<IBGEUFS[]>([]);
+  const [selectedUf, setSelectedUf] = useState<number>(0);
+  const [allCities, setAllCities] = useState<IBGECITIES[]>([]);
+  const [selectedCity, setSelectedCity] = useState<string>("");
+  const [selectedLatLng, setSelectedLatLng] = useState<LATLNG>({
+    lat: -21.5383665,
+    lng: -43.0097713,
+  });
   useEffect(() => {
     getItems();
+    getUfs();
   }, []);
+
+  useEffect(() => {
+    getCitiesByUf();
+  }, [selectedUf]);
 
   const getItems = async () => {
     const response = await api.get("/items");
     setItems(response.data.items);
+  };
+
+  const getUfs = async () => {
+    const response = await ibge.get("/estados");
+    const realUf = response.data.map((uf: any) => {
+      return {
+        name: uf.nome,
+        sigla: uf.sigla,
+        region: uf.regiao,
+        id: uf.id,
+      };
+    });
+    setUfs(realUf);
+  };
+
+  const getCitiesByUf = async () => {
+    if (selectedUf) {
+      const response = await ibge.get(`/estados/${selectedUf}/municipios`);
+      const realData = response.data.map((item: any) => {
+        return {
+          id: item.id,
+          name: item.nome,
+        };
+      });
+      setAllCities(realData);
+    }
+  };
+
+  const filterArrayItem = (item: number, id: number) => {
+    return item !== id;
+  };
+
+  const addSelectedItem = (id: number) => {
+    const placeholderSelectedItem = selectedItemsArray;
+    if (!placeholderSelectedItem.includes(id)) {
+      setSelectedItemsArray(placeholderSelectedItem.concat(id));
+    } else {
+      const placeholder: number[] = [];
+      placeholderSelectedItem.forEach((item) => {
+        if (item !== id) {
+          placeholder.push(item);
+        }
+      });
+      setSelectedItemsArray(placeholder);
+    }
+  };
+
+  const onMapClick = (event: LeafletMouseEvent) => {
+    setSelectedLatLng(event.latlng);
   };
 
   return (
@@ -70,17 +156,47 @@ const CreatePoint = () => {
             <span>Selecione o endereço no mapa</span>
           </legend>
 
+          <Map
+            center={[selectedLatLng.lat, selectedLatLng.lng]}
+            zoom={15}
+            onClick={onMapClick}
+          >
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[selectedLatLng.lat, selectedLatLng.lng]} />
+          </Map>
+
           <div className="field-group">
             <div className="field">
               <label htmlFor="uf">Estado (UF)</label>
-              <select name="uf" id="uf">
-                <option value="0">Selecione uma UF</option>
+              <select
+                onChange={(e) => setSelectedUf(parseFloat(e.target.value))}
+                name="uf"
+                id="uf"
+              >
+                {ufs.map((uf) => {
+                  console.log(uf);
+                  return (
+                    <option key={uf.id} value={uf.id}>
+                      {uf.name}
+                    </option>
+                  );
+                })}
               </select>
             </div>
             <div className="field">
-              <label htmlFor="city">Estado (UF)</label>
-              <select name="city" id="city">
-                <option value="0">Selecione uma cidade</option>
+              <label htmlFor="city">Cidade</label>
+              <select
+                onChange={(e) => setSelectedCity(e.target.value)}
+                value={selectedCity}
+                name="city"
+                id="city"
+              >
+                {allCities.map((city) => (
+                  <option value={city.id}>{city.name}</option>
+                ))}
               </select>
             </div>
           </div>
@@ -93,7 +209,13 @@ const CreatePoint = () => {
           <ul className="items-grid">
             {items &&
               items.map((item) => (
-                <li key={item.id}>
+                <li
+                  className={
+                    selectedItemsArray.includes(item.id) ? "selected" : ""
+                  }
+                  onClick={() => addSelectedItem(item.id)}
+                  key={item.id}
+                >
                   <img src={`http://localhost:3333/files/${item.image}`}></img>
                   <span>{item.title}</span>
                 </li>
